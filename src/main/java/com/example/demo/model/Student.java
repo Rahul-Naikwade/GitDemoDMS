d4054f1313d91bf6bae1285517be846ac69f5650
tusharwaghulde <55015989+tusharwaghulde@users.noreply.github.com>
2019-09-09 14:15:39 +0530

    fields added to student

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,5 +1,18 @@
 package com.example.demo.model;
 
 public class Student {
-
+	private int rollno;
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
 }
