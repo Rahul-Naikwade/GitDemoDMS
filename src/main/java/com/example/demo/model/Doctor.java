bfe00f35721360e147db4bfc8f3b373f0297dd61
tusharwaghulde <55015989+tusharwaghulde@users.noreply.github.com>
2019-09-09 14:23:47 +0530

    docter pojo added

---
+++ src/main/java/com/example/demo/model/Doctor.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+public class Doctor {
+	private int docId;
+	private String docName;
+	public int getDocId() {
+		return docId;
+	}
+	public void setDocId(int docId) {
+		this.docId = docId;
+	}
+	public String getDocName() {
+		return docName;
+	}
+	public void setDocName(String docName) {
+		this.docName = docName;
+	}
+
+}
